48ab9c35f8911c2fea6457f6f85a241c3001252f
Jason Yu <me@ycmjason.com>
2019-01-25 14:37:10 +0000

    updated test names to fix snapshot

--- packages/@frograming/language/src/__tests__/parse.test.js
+++ packages/@frograming/language/src/__tests__/parse.test.js
@@ -1,5 +1,5 @@
 import { readFile, readdirSync } from 'fs-extra';
-import { join } from 'path';
+import { join, basename } from 'path';
 import parse from '../parse';
 
 const readdirFullSync = (dir) => readdirSync(dir).map(name => join(dir, name));
@@ -7,7 +7,7 @@ const readdirFullSync = (dir) => readdirSync(dir).map(name => join(dir, name));
 describe('parser', () => {
   readdirFullSync(join(__dirname, './frogram-specs/frogs'))
     .filter(n => /.frog$/.test(n))
-    .forEach(path => it(path, async () => {
+    .forEach(path => it(basename(path), async () => {
       expect(parse(await readFile(path, 'utf8'))).toMatchSnapshot();
     }));
 });
