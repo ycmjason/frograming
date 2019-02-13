09c0b144b6ec1f9a3265bc6c3aaa867cdf07b3eb
Jason Yu <me@ycmjason.com>
2019-02-13 18:06:35 +0000

    added code mirror

--- packages/frograming-web/src/components/Editor.vue
+++ packages/frograming-web/src/components/Editor.vue
@@ -1,21 +1,35 @@
 <template>
-  <textarea
+  <codemirror
       data-gramm_editor="false"
+      class="editor"
+      :options="options"
       :value="value"
-      @input="$emit('input', $event.target.value)"></textarea>
+      @input="$emit('input', $event)" />
 </template>
 
 <script>
+import { codemirror } from 'vue-codemirror';
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/base16-dark.css';
+
 export default {
+  components: { codemirror },
   props: ['value'],
+  data: () => ({
+    options: {
+      tabSize: 4,
+      mode: 'text/javascript',
+      theme: 'base16-dark',
+      lineNumbers: true,
+      line: true,
+    },
+  }),
 };
 </script>
 
 <style scoped>
-textarea {
+.editor {
   width: 100%;
   height: 100%;
-  resize: none;
-  padding: 1rem;
 }
 </style>
--- packages/frograming-web/src/main.js
+++ packages/frograming-web/src/main.js
@@ -1,13 +1,9 @@
 import Vue from 'vue';
-// import { sync } from 'vuex-router-sync';
 
 import App from './App.vue';
 import router from './router';
-// import store from './store';
 
 import { FadeTransition, SlideYDownTransition } from 'vue2-transitions';
-
-// sync(router, store);
 
 Vue.config.productionTip = false;
 
@@ -16,6 +12,5 @@ Vue.component('SlideYDownTransition', SlideYDownTransition);
 
 new Vue({
   router,
-  // store,
   render: h => h(App),
 }).$mount('#app');
