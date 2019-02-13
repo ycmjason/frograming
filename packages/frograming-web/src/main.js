897f722867c06e340997af04f960dd6364bc5425
Jason Yu <me@ycmjason.com>
2019-02-13 22:58:48 +0000

    added google analytics

--- packages/frograming-web/src/main.js
+++ packages/frograming-web/src/main.js
@@ -3,9 +3,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import VueAnalytics from 'vue-analytics';
+
 import { FadeTransition, SlideYDownTransition } from 'vue2-transitions';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, { id: 'UA-113352609-6', router });
 
 Vue.component('FadeTransition', FadeTransition);
 Vue.component('SlideYDownTransition', SlideYDownTransition);
