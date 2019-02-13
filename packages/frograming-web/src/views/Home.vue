4d12e3a35ff86e7d2c68324d91d7834cae8e7bef
Jason Yu <me@ycmjason.com>
2019-02-13 22:44:51 +0000

    added introduction to the home page

--- packages/frograming-web/src/views/Home.vue
+++ packages/frograming-web/src/views/Home.vue
@@ -1,14 +1,38 @@
 <template>
   <div class="container">
-    Welcome to Frogram. <router-link to="/l/0">Go to the first level</router-link>!
+    <h2>Introduction</h2>
+
+    <p>Welcome to Frogram.</p>
+
+    <p>Frogram is a website aiming to introduce programming in a fun and easy way. You will go through 3 levels of challenges. All you have to do is to guide the frog to the "GOAL".</p>
+
+    <p>This website currently is mainly designed to work best with computers.</p>
+
+    <KeyboardFrogger />
+
+    <p>
+      Go to the <router-link to="/l/0">first level</router-link> now!
+    </p>
   </div>
 </template>
 
 <script>
+import KeyboardFrogger from '@/components/KeyboardFrogger.vue';
+
 export default {
   name: 'home',
+  components: { KeyboardFrogger },
 };
 </script>
 
 <style lang="scss" scoped>
+h2 {
+  text-align: center;
+}
+
+p {
+  max-width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+}
 </style>
